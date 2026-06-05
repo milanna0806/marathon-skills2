82f6b36b72f372dff5e334922a8c4df01acb1845
milanna0806 <milannaigorevna@gmail.com>
2026-06-05 13:12:02 +0500

    Add files via upload

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,33 @@
+// pages/api/auth/[...nextauth].js
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ session, token }) {
+      session.user.id = token.sub;
+      return session;
+    },
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+};
+
+export default NextAuth(authOptions);
